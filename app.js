07c31fea6d00ec2ab1870fdc8c95d1a98b52a8cc
sushantshirsekar <sushantshirsekar60@gmail.com>
2023-05-17 20:14:49 +0530

    Express task-1

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
-const http = require('http'); 
+const express = require('express');
 
-const server = http.createServer((req,res)=> {
-    const url = req.url; 
-    res.setHeader('Content-Type', 'text/html'); 
-    if(url === "/"){
-        res.write('<html>'); 
-        res.write('<head><title>Helloooo!</title></head>');
-        res.write('<body><h1>Welcome to default page</h1></body>');  
-        res.write('</html>'); 
-    }
+const app = express(); 
 
-    if(url === "/home"){
-        res.write('<html>'); 
-        res.write('<head><title>Helloooo!</title></head>');
-        res.write('<body><h1>Welcome to home page</h1></body>');  
-        res.write('</html>'); 
-    }
-
-    if(url === "/about"){
-        res.write('<html>'); 
-        res.write('<head><title>Helloooo!</title></head>');
-        res.write('<body><h1>Welcome to About us page</h1></body>');  
-        res.write('</html>'); 
-    }
-
-    if(url === "/node"){
-        res.write('<html>'); 
-        res.write('<head><title>Helloooo!</title></head>');
-        res.write('<body><h1>Welcome to my node project</h1></body>');  
-        res.write('</html>'); 
-    }
-    res.end();
-    
+app.use((req, res,next)=> {
+    console.log("In the middleware!");
+    next(); 
 }); 
 
-server.listen(3000); 
+app.use((req, res,next)=> {
+    console.log("Another"); 
+    res.send({key:"value"});
+});
+
+app.listen(3000);
